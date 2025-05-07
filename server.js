8d6eba7625363492394b88a8489fc3ec7279a440
kharchenkok <ekharchenko0108@gmail.com>
2025-05-07 21:54:34 +0300

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
 
 app.get('/get-gender', async (req, res) => {
     const name = req.query.name;
